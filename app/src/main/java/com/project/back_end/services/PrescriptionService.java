c18667868424aa9ec396c9b9e89c9d762847f8e4
papalum <papalymp@gmail.com>
2025-12-11 17:04:58 +0200

    Update PrescriptionService.java

--- app/src/main/java/com/project/back_end/services/PrescriptionService.java
+++ app/src/main/java/com/project/back_end/services/PrescriptionService.java
@@ -1,7 +1,7 @@
 package com.project.back_end.services;
 
 import com.project.back_end.models.Prescription;
-import com.project.back_end.repositories.PrescriptionRepository;
+import com.project.back_end.repo.PrescriptionRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
