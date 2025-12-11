314d29a887c10b3dce0d840e79cf7f47fcc5262a
papalum <papalymp@gmail.com>
2025-12-11 22:49:48 +0200

    Update DashboardController.java

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import com.project.back_end.services.ServiceManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import java.util.Map;
 
