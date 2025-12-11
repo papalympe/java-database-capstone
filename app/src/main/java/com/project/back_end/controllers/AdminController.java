ba0ac7942f425de4dc84ee6e8072ca7b528a715e
papalum <papalymp@gmail.com>
2025-12-11 16:48:25 +0200

    Update AdminController.java

--- app/src/main/java/com/project/back_end/controllers/AdminController.java
+++ app/src/main/java/com/project/back_end/controllers/AdminController.java
@@ -13,16 +13,10 @@ public class AdminController {
 
     private final Service service;
 
-    // Constructor Injection για το Service
     public AdminController(Service service) {
         this.service = service;
     }
 
-    /**
-     * Admin Login Endpoint
-     * @param admin Το αντικείμενο Admin που περιέχει username και password
-     * @return ResponseEntity με token ή μήνυμα σφάλματος
-     */
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> adminLogin(@RequestBody Admin admin) {
         return service.validateAdmin(admin);
