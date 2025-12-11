8d37192cac28089359675e1d3f1e183dca221f55
papalum <papalymp@gmail.com>
2025-12-11 20:56:46 +0200

    Update PatientRepository.java

--- app/src/main/java/com/project/back_end/repo/PatientRepository.java
+++ app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -3,12 +3,13 @@ package com.project.back_end.repo;
 import com.project.back_end.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     // Find a patient by email
-    Optional<Patient> patientOpt = Optional.ofNullable(patientRepository.findByEmail(email));
+    Optional<Patient> findByEmail(String email);
 
     // Find a patient by email or phone
     Optional<Patient> findByEmailOrPhone(String email, String phone);
