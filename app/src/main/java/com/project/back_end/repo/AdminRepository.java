47160c3f6134933ba91c3dea8f75b4ec70ad4d42
papalum <papalymp@gmail.com>
2025-12-11 15:06:31 +0200

    Update AdminRepository.java

--- app/src/main/java/com/project/back_end/repo/AdminRepository.java
+++ app/src/main/java/com/project/back_end/repo/AdminRepository.java
@@ -1,17 +1,12 @@
 package com.project.back_end.repo;
 
-import com.project.back_end.models.Admin;
+import com.project.back_end.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
     // Find an admin by username
-    Optional<Admin> findByUsername(String username);
-
-    // Find an admin by email (for token validation)
-    Optional<Admin> findByEmail(String email);
+    Admin findByUsername(String username);
 }
