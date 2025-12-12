d6becdb4a9f48696f4551a9f1672919d97323e35
papalum <papalymp@gmail.com>
2025-12-12 15:40:35 +0200

    Update Doctor.java

--- app/src/main/java/com/project/back_end/models/Doctor.java
+++ app/src/main/java/com/project/back_end/models/Doctor.java
@@ -30,7 +30,7 @@ public class Doctor {
     @Pattern(regexp = "\\d{10}", message = "Phone number must be 10 digits")
     private String phone;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<String> availableTimes;
 
     // Getters and Setters
