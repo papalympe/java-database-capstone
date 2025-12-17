a603c0e0a4a071fbe425d490c96a15a12945c8ab
papalum <papalymp@gmail.com>
2025-12-17 18:14:34 +0200

    Update util.js

--- app/src/main/resources/static/js/util.js
+++ app/src/main/resources/static/js/util.js
@@ -1,13 +1,21 @@
 // util.js
-  function setRole(role) {
-    localStorage.setItem("userRole", role);
-  }
-  
-  function getRole() {
-    return localStorage.getItem("userRole");
-  }
-  
-  function clearRole() {
-    localStorage.removeItem("userRole");
-  }
+// small helpers for storing role in localStorage
+
+function setRole(role) {
+  localStorage.setItem("userRole", role);
+}
+
+function getRole() {
+  return localStorage.getItem("userRole");
+}
+
+function clearRole() {
+  localStorage.removeItem("userRole");
+}
+
+// expose on window so other modules/non-module scripts can call them safely
+window.setRole = setRole;
+window.getRole = getRole;
+window.clearRole = clearRole;
+
   
