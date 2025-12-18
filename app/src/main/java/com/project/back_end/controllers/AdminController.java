35557720e689c683984d0107fc72d1968d199b20
papalum <papalymp@gmail.com>
2025-12-18 12:49:07 +0200

    Update AdminController.java

--- app/src/main/java/com/project/back_end/controllers/AdminController.java
+++ app/src/main/java/com/project/back_end/controllers/AdminController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("${api.path}admin")
 public class AdminController {
