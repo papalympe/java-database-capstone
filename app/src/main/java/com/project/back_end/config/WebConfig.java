041483e841e452c55fcdba6619e974a1baadc386
papalum <papalymp@gmail.com>
2025-12-18 10:15:20 +0200

    Update WebConfig.java

--- app/src/main/java/com/project/back_end/config/WebConfig.java
+++ app/src/main/java/com/project/back_end/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             // στις νεότερες εκδόσεις: allowedOriginPatterns επιτρέπει wildcards μαζί με credentials
-            .allowedOriginPatterns("https://kpapalum-8080.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai", "https://your-other-origin.example.com")
+            .allowedOriginPatterns("https://kpapalum-8080.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai", "*")
             .allowedMethods("GET","POST","PUT","DELETE","OPTIONS","PATCH")
             .allowedHeaders("*")
             .allowCredentials(true)
